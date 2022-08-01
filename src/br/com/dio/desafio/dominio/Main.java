5327e0b0b7b7c22812d758931713d16946b7a308
LuanBarb0sa <luanlucas.r10@hotmail.com>
2022-08-01 16:30:25 -0300

    Interligando as classes de domínio

--- src/br/com/dio/desafio/dominio/Main.java
+++ src/br/com/dio/desafio/dominio/Main.java
@@ -25,10 +25,39 @@ public class Main {
 		Conteudo conteudo = new Curso();
 		Conteudo conteudo1 = new Mentoria();
 		
-		System.out.println(curso1);
-		System.out.println(curso2);
-		System.out.println(mentoria);
-
+		/*
+		 * System.out.println(curso1); System.out.println(curso2);
+		 * System.out.println(mentoria);
+		 */
+		
+		Bootcamp bootcamp = new Bootcamp();
+		bootcamp.setNome("Bootcamp Java Developer");
+		bootcamp.setDescricao("Aprimorando conceitos Java");
+		bootcamp.getConteudos().add(curso1);
+		bootcamp.getConteudos().add(curso2);
+		bootcamp.getConteudos().add(mentoria);
+		
+		Dev devLuan = new Dev ();
+		devLuan.setNome("Luan Lucas Barbosa");
+		devLuan.inscreverBootcamp(bootcamp);
+		System.out.println("Conteúdos inscritos Luan:" + devLuan.getConteudosInscritos());
+		devLuan.progredir();
+		System.out.println("--");
+		System.out.println("Conteúdos inscritos Luan:" + devLuan.getConteudosInscritos());
+		System.out.println("Conteúdos concluídos Luan:" + devLuan.getConteudosConcluidos());
+		System.out.println("XP:" + devLuan.calcularTotalXp());
+		
+		Dev devYuri = new Dev();
+		devYuri.setNome("Yuri Klissman Barbosa");
+		devYuri.inscreverBootcamp(bootcamp);
+		System.out.println("Conteúdos inscritos Yuri:" + devYuri.getConteudosInscritos());
+		devYuri.progredir();
+		devYuri.progredir();
+		devYuri.progredir();
+		System.out.println("--");
+		System.out.println("Conteúdos inscritos Yuri:" + devYuri.getConteudosInscritos());
+		System.out.println("Conteúdos concluídos Yuri:" + devYuri.getConteudosConcluidos());
+		System.out.println("XP:" + devYuri.calcularTotalXp());
 	}
 
 }
